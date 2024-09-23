12894a313ca3d4db474143af40299052a4ec00be
Xhakah <140828371+Xhakah@users.noreply.github.com>
2024-09-23 11:53:04 -0300

    Update questao4.js

--- questao4.js
+++ questao4.js
@@ -6,10 +6,10 @@ let faturamentoEstados = {
     'Outros': 19849.53
 };
 
-// Calcular o faturamento total
+
 let faturamentoTotal = Object.values(faturamentoEstados).reduce((a, b) => a + b, 0);
 
-// Calcular e exibir o percentual de representação de cada estado
+
 for (let estado in faturamentoEstados) {
     let percentual = (faturamentoEstados[estado] / faturamentoTotal) * 100;
     console.log(`${estado}: ${percentual.toFixed(2)}%`);
